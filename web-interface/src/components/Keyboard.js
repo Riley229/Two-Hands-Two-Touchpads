f89a0754f7372a86bad45bbb08e8b020dba62b84
Riley229 <thomsoncamden@gmail.com>
2023-09-22 11:27:32 -0500

    Added simplified keyboard layout, implemented capslock, added event functions

--- web-interface/src/components/Keyboard.js
+++ web-interface/src/components/Keyboard.js
@@ -4,15 +4,34 @@ import { KeyboardBackspace, ArrowUpward, KeyboardTab, KeyboardArrowLeft, Keyboar
 import "./Keyboard.css"
 
 import KeyboardButton from "./KeyboardButton";
-import keyboardLayout from "../data/KeyboardLayout";
+import SimplifiedLayout from "../data/SimplifiedLayout";
+import StandardLayout from "../data/StandardLayout";
 
 class Keyboard extends React.Component {
+  static simplifiedLayout = true;
+
   constructor(props) {
     super(props);
 
     this.state = {
       uppercase: false,
+      capsLock: false,
     };
+  }
+
+  getLayout() {
+    if (Keyboard.simplifiedLayout)
+      return SimplifiedLayout;
+    else
+      return StandardLayout;
+  }
+
+  onCapsLock() {
+    const { capsLock } = this.state;
+
+    this.setState({
+      capsLock: !capsLock,
+    });
   }
 
   onShiftClick() {
@@ -23,12 +42,35 @@ class Keyboard extends React.Component {
     });
   }
 
-  render() {
+  onBackspace() {
+    // TODO: implement
+  }
+
+  onEnter() {
+    // TODO: implement
+  }
+
+  onNavigate(left) {
+    // TODO: implement
+  }
+
+  onKeyClick(key) {
     const { uppercase } = this.state;
-    const keys = keyboardLayout;
+
+    // TODO: implement
+
+    if (uppercase) 
+      this.setState({
+        uppercase: false,
+      });
+  }
+
+  render() {
+    const { uppercase, capsLock } = this.state;
+    const keys = this.getLayout();
 
     return (
-      <div className="keyboard">
+      <div className={`keyboard ${Keyboard.simplifiedLayout ? 'simplified-keyboard' : 'standard-keyboard'}`}>
         {keys.map((row, i) => {
           var shiftRow = false;
           if (row.includes("*sh"))
@@ -40,60 +82,62 @@ class Keyboard extends React.Component {
                 case "*bs":
                   return <KeyboardButton 
                     value={<KeyboardBackspace />}
-                    onClick={() => true}
-                    classes="fill-key"
+                    onClick={() => this.onBackspace()}
+                    classes="stretch-key control-key"
                   />;
                 case "*sh":
                   return <KeyboardButton 
                     value={<ArrowUpward />}
                     onClick={() => this.onShiftClick()}
-                    classes="fill-key"
+                    classes="stretch-key control-key"
                   />;
                 case "*sp":
                   return <KeyboardButton
                     value=""
-                    onClick={() => true}
-                    classes="fill-key"
+                    onClick={() => this.onKeyClick(" ")}
+                    classes="stretch-key"
                   />;
                 case "*tb":
                   return <KeyboardButton 
                     value={<KeyboardTab />}
-                    onClick={() => true}
-                    classes="fill-key"
+                    onClick={() => this.onKeyClick("\t")}
+                    classes="stretch-key control-key"
                   />;
                 case "*cps":
                   return <KeyboardButton 
                     value="CAPS"
-                    onClick={() => true}
-                    classes="fill-key"
+                    onClick={() => this.onCapsLock()}
+                    classes="stretch-key control-key"
                   />;
                 case "*e":
                   return <KeyboardButton 
                     value={<SubdirectoryArrowLeft />}
-                    onClick={() => true}
-                    classes="fill-key"
+                    onClick={() => this.onEnter()}
+                    classes="stretch-key control-key"
                   />;
                 case "\\":
                   return <KeyboardButton
                     value="\"
-                    onClick={() => true}
-                    classes="fill-key"
+                    onClick={() => this.onKeyClick(value)}
+                    classes="stretch-key"
                   />;
                 case "*l":
                   return <KeyboardButton 
                     value={<KeyboardArrowLeft />}
-                    onClick={() => true}
+                    onClick={() => this.onNavigate(true)}
+                    classes="control-key"
                   />;
                 case "*r":
                   return <KeyboardButton 
                     value={<KeyboardArrowRight />}
-                    onClick={() => true}
+                    onClick={() => this.onNavigate(false)}
+                    classes="control-key"
                   />;
                 default:
-                  const value = uppercase ? button.toUpperCase() : button.toLowerCase();
+                  const value = (uppercase || capsLock) ? button.toUpperCase() : button.toLowerCase();
                   return <KeyboardButton
                     value={value}
-                    onClick={() => true}
+                    onClick={() => this.onKeyClick(value)}
                   />
               }
             })}
